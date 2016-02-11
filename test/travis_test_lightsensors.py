922894636af1e26b4f4979f485c17e07745e7a15
tide1218 <s91424@infoseek.jp>
2016-02-12 06:06:17 +0900

    Change test_lightsensors.py
    mistyping

--- test/travis_test_lightsensors.py
+++ test/travis_test_lightsensors.py
@@ -8,7 +8,7 @@ from pimouse_ros.msg import LightSensorValues
 class LightsensorTest(unittest.TestCase):
     def setUp(self):
         self.count = 0
-        rospy.Subscriver('/lightsensors', LightSensorValues, self.callback)
+        rospy.Subscriber('/lightsensors', LightSensorValues, self.callback)
         self.values = LightSensorValues()
 
     def callback(self,data):
@@ -25,7 +25,7 @@ class LightsensorTest(unittest.TestCase):
         self.assertEqual(vs.sum_forward, lf+rf, "different value: sum_forward")
 
     def test_node_exist(self):
-        nodes = rosnode.get_nodenames()
+        nodes = rosnode.get_node_names()
         self.assertIn('/lightsensors',nodes, "node does not exist")
 
     def test_get_value(self):
@@ -50,5 +50,5 @@ class LightsensorTest(unittest.TestCase):
 
 if __name__ == '__main__':
     time.sleep(3)
-    rospy.init_node('travis_test_lightsensor')
+    rospy.init_node('travis_test_lightsensors)
     rostest.rosrun('pimouse_ros','travis_test_lightsensors',LightsensorTest)
